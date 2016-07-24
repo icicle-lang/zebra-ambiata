8c05244ceb803a0cbefad1f6aa1be008e38fc6ba
Amos Robinson <amos.robinson@gmail.com>
2016-07-25 09:30:19 +1000

    Reenable Block and Fact tests

--- test/test.hs
+++ test/test.hs
@@ -1,5 +1,7 @@
 import           Disorder.Core.Main
 
+import qualified Test.Zebra.Data.Block
+import qualified Test.Zebra.Data.Fact
 import qualified Test.Zebra.Data.Record
 import qualified Test.Zebra.Data.Schema
 import qualified Test.Zebra.Serial.Array
@@ -10,7 +12,9 @@ import qualified Test.Zebra.Merge.Entity
 main :: IO ()
 main =
   disorderMain [
-      Test.Zebra.Data.Record.tests
+      Test.Zebra.Data.Block.tests
+    , Test.Zebra.Data.Fact.tests
+    , Test.Zebra.Data.Record.tests
     , Test.Zebra.Data.Schema.tests
     , Test.Zebra.Serial.Array.tests
     , Test.Zebra.Serial.Block.tests
