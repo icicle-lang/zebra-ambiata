5e2fea1da73475234cb080346724dbf372f0ba25
Amos Robinson <amos.robinson@gmail.com>
2016-07-25 17:25:00 +1000

    trying out better repr for merging

--- src/Zebra/Merge/Entity.hs
+++ src/Zebra/Merge/Entity.hs
@@ -1,31 +1,62 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeFamilies #-}
 module Zebra.Merge.Entity
-  ( EntityValues(..)
+  ( BlockDataId(..)
+  , EntityValues(..)
   , entitiesOfBlock
+  , mergeEntityValues
   ) where
 
 import Zebra.Data
 
+import           Data.Vector.Unboxed.Deriving (derivingUnbox)
 import qualified X.Data.Vector as Boxed
 import qualified X.Data.Vector.Unboxed as Unboxed
 import qualified X.Data.Vector.Generic as Generic
+import qualified X.Data.Vector.Stream as Stream
 
 import qualified X.Text.Show as Show
 import GHC.Generics (Generic(..))
 
 import P
 
+
+-- | A BlockDataId roughly corresponds to the id of the file, where a Record came from.
+newtype BlockDataId
+ = BlockDataId
+ { unDataChunkId :: Int
+ } deriving (Eq, Ord, Generic)
+instance Show BlockDataId where
+ showsPrec = Show.gshowsPrec
+
+derivingUnbox "BlockDataId"
+  [t| BlockDataId -> Int |]
+  [| \(BlockDataId x) -> x |]
+  [| \x -> BlockDataId x |]
+
+
+-- | EntityValues is a single entity and all its values.
+-- It has multiple chunks of Records, one for each input file and attribute.
+-- When merging entities from many files we don't want to chop the data up
+-- and cat it back together multiple times.
+-- So it's better to just store the raw, unchopped records from each input file,
+-- and each index also stores which file it came from.
+-- Then after all files have been merged, we can chop all input files once, merge them
+-- according to the indices, then concatenate.
 data EntityValues
  = EntityValues
  { evEntity    :: !Entity
- , evIndices   :: !(Boxed.Vector (Unboxed.Vector Index))
+ , evIndices   :: !(Boxed.Vector (Unboxed.Vector (Index, BlockDataId)))
    -- ^ Indexes for current entity, indexed by attribute id
- , evRecords   :: !(Boxed.Vector Record)
+ , evRecords   :: !(Boxed.Vector (Boxed.Vector (BlockDataId, Record)))
    -- ^ Record values for current entity, indexed by attribute id
  }
  deriving (Eq, Generic)
@@ -33,8 +64,8 @@ instance Show EntityValues where
  showsPrec = Show.gshowsPrec
 
 
-entitiesOfBlock :: Block -> Boxed.Vector EntityValues
-entitiesOfBlock (Block entities indices records) =
+entitiesOfBlock :: BlockDataId -> Block -> Boxed.Vector EntityValues
+entitiesOfBlock blockId (Block entities indices records) =
   Boxed.mapAccumulate go (indices,records) entities
   where
     go (ix,rx) ent
@@ -47,8 +78,10 @@ entitiesOfBlock (Block entities indices records) =
            dense_counts      = Boxed.map countNotTombstone ix_attrs
            (rx_here,rx_rest) = Boxed.unzip $ Boxed.zipWith splitAtRecords dense_counts rx
 
-           acc' = (ix_rest, rx_rest)
-           ev   = EntityValues ent ix_attrs rx_here
+           acc'              = (ix_rest, rx_rest)
+           ix_blockId        = Boxed.map (Unboxed.map (,blockId)) ix_attrs
+           rx_blockId        = Boxed.map (Boxed.singleton . (blockId,)) rx_here
+           ev   = EntityValues ent ix_blockId rx_blockId
        in (acc', ev)
 
 
@@ -82,4 +115,29 @@ denseAttributeCount rs attr =
      | otherwise
      = (attrs, 0)
 
+
+mergeEntityValues :: Monad m => Stream.Stream m EntityValues -> Stream.Stream m EntityValues -> Stream.Stream m EntityValues
+mergeEntityValues ls rs
+ = Stream.merge (Stream.mergePullJoin joinEV ordEV) ls rs
+ where
+  -- id and hash are equal
+  joinEV e1 e2
+   = let evIxs = Boxed.zipWith mergeIxs (evIndices e1) (evIndices e2)
+         evRcs = Boxed.zipWith mergeRcs (evRecords e1) (evRecords e2)
+     in  Stream.MergePullBoth e1 { evIndices = evIxs, evRecords = evRcs }
+
+  mergeIxs
+   = Unboxed.merge (Stream.mergePullOrd (\(i,_) -> (indexTime i, indexPriority i)))
+  mergeRcs
+   = Boxed.merge (Stream.mergePullOrd fst)
+
+  ordEV ev
+   = let e = evEntity ev
+     in  (entityHash e, entityId e)
+
+-- mergeRecords: gather and concatenate all the records from different blocks.
+-- This should be done after all the indices have been merged, so that it only has to
+-- slice and concat the actual data once, instead of for each pair of merges.
+--
+-- mergeRecords :: EntityValues -> EntityValues
 
--- test/Test/Zebra/Merge/Entity.hs
+++ test/Test/Zebra/Merge/Entity.hs
@@ -16,19 +16,23 @@ import           Zebra.Merge.Entity
 
 import qualified X.Data.Vector as Boxed
 
+fakeBlockId :: BlockDataId
+fakeBlockId = BlockDataId 0
+
 
 prop_entitiesOfBlock_entities :: Property
 prop_entitiesOfBlock_entities =
   gamble jBlock $ \block ->
-    fmap evEntity (entitiesOfBlock block) === blockEntities block
+    fmap evEntity (entitiesOfBlock fakeBlockId block) === blockEntities block
 
 prop_entitiesOfBlock_indices :: Property
 prop_entitiesOfBlock_indices =
   gamble jBlockValid $ \block ->
-    catIndices (entitiesOfBlock block) === takeIndices block
+    catIndices (entitiesOfBlock fakeBlockId block) === takeIndices block
  where
   catIndices evs
-   = Boxed.concatMap Boxed.convert
+   = Boxed.map fst
+   $ Boxed.concatMap Boxed.convert
    $ Boxed.concatMap evIndices evs
 
   takeIndices block
