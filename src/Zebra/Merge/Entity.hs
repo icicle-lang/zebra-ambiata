d07404cb24bab332fabdaa53f8569087c8c45d9c
Amos Robinson <amos.robinson@gmail.com>
2016-08-08 11:50:38 +1000

    more merges

--- src/Zebra/Merge/Entity.hs
+++ src/Zebra/Merge/Entity.hs
@@ -5,87 +5,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeFamilies #-}
 module Zebra.Merge.Entity
-  ( BlockDataId(..)
-  , EntityValues(..)
-  , entitiesOfBlock
+  ( entitiesOfBlock
   , mergeEntityValues
   , mergeEntityRecords
-  , extractEntityValues
+  , entityMergedOfEntityValues
   ) where
 
 import Zebra.Data
+import Zebra.Merge.Base
 
 import qualified Data.Map as Map
 
-import           Data.Vector.Unboxed.Deriving (derivingUnbox)
 import qualified X.Data.Vector as Boxed
 import qualified X.Data.Vector.Unboxed as Unboxed
 import qualified X.Data.Vector.Generic as Generic
 import qualified X.Data.Vector.Stream as Stream
 
-import qualified X.Text.Show as Show
-import GHC.Generics (Generic(..))
-
 import P
 
 
--- | A BlockDataId roughly corresponds to the id of the file, where a Record came from.
-newtype BlockDataId
- = BlockDataId
- { unDataChunkId :: Int
- } deriving (Eq, Ord, Generic)
-instance Show BlockDataId where
- showsPrec = Show.gshowsPrec
 
-derivingUnbox "BlockDataId"
-  [t| BlockDataId -> Int |]
-  [| \(BlockDataId x) -> x |]
-  [| \x -> BlockDataId x |]
-
-data MergeError =
-    MergeRecordError !RecordError
-  | MergeAttributeWithoutRecord !AttributeId
-  | MergeBlockDataWithoutRecord !AttributeId !BlockDataId
-    deriving (Eq, Show)
-
-
--- | EntityValues is a single entity and all its values.
--- It has multiple chunks of Records, one for each input file and attribute.
--- When merging entities from many files we don't want to chop the data up
--- and cat it back together multiple times.
--- So it's better to just store the raw, unchopped records from each input file,
--- and each index also stores which file it came from.
--- Then after all files have been merged, we can chop all input files once, merge them
--- according to the indices, then concatenate.
---
--- Invariants: 
---   * length evIndicies == length evRecords
---      (evIndices and evRecords are same length)
---   * forall i, length (evRecords ! i) > 0
---      (each attribute has at least one Record)
---   * forall i j, snd (evIndicies ! i ! j) `Map.member` evRecords ! i
---      (each BlockDataId in evIndices refers to a BlockDataId in evRecords)
---      (the converse isn't true: there can be Records that have no indices)
---
-data EntityValues
- = EntityValues
- { evEntity    :: !Entity
- , evIndices   :: !(Boxed.Vector (Unboxed.Vector (Index, BlockDataId)))
-   -- ^ Indexes for current entity, indexed by attribute id
- , evRecords   :: !(Boxed.Vector (Map.Map BlockDataId Record))
-   -- ^ Record values for current entity, indexed by attribute id
- }
- deriving (Eq, Generic)
-instance Show EntityValues where
- showsPrec = Show.gshowsPrec
-
-
-entitiesOfBlock :: BlockDataId -> Block -> Boxed.Vector EntityValues
+entitiesOfBlock :: Monad m => BlockDataId -> Block -> Stream.Stream m EntityValues
 entitiesOfBlock blockId (Block entities indices records) =
+  Stream.streamOfVectorM $
   Boxed.mapAccumulate go (indices,records) entities
   where
     go (ix,rx) ent
@@ -192,8 +137,8 @@ mergeEntityRecord aid aixs records = do
      = first MergeRecordError $ appendRecords a b
 
 
-extractEntityValues :: EntityValues -> Either MergeError (EntityHash, EntityId, Boxed.Vector (Unboxed.Vector Index), Boxed.Vector Record)
-extractEntityValues ev@(EntityValues e aixs _) = do
+entityMergedOfEntityValues :: EntityValues -> Either MergeError EntityMerged
+entityMergedOfEntityValues ev@(EntityValues e aixs _) = do
   recs <- mergeEntityRecords ev
-  return (entityHash e, entityId e, Boxed.map (Unboxed.map fst) aixs, recs)
+  return $ EntityMerged (entityHash e) (entityId e) (Boxed.map (Unboxed.map fst) aixs) recs
 
