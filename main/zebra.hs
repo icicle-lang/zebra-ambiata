0346c1474c46ec5a32e2b46783811a488daecf84
Amos Robinson <amos.robinson@gmail.com>
2016-08-18 12:36:46 +1000

    Rough first-pass implementation of merging

--- main/zebra.hs
+++ main/zebra.hs
@@ -6,6 +6,8 @@ import           DependencyInfo_ambiata_zebra
 
 import           Zebra.Data.Block
 import           Zebra.Serial.File
+import           Zebra.Merge.Base
+import           Zebra.Merge.Block
 
 import           Options.Applicative
 
@@ -19,6 +21,7 @@ import qualified X.Data.Vector.Stream as Stream
 
 import           Data.String (String)
 import           System.IO
+import           System.IO.Unsafe (unsafePerformIO)
 import           Data.IORef
 import           System.Exit
 import           X.Options.Applicative
@@ -114,8 +117,46 @@ run c = case c of
       putStrLn "Header information:"
       putStrLn (show r)
 
-  MergeFiles _ins _out ->
-    putStrLn "*implement me*" >> exitFailure
+  MergeFiles [] _out -> do
+    putStrLn "Merge: No files"
+    exitFailure
+
+  MergeFiles ins@(i:_) _out -> do
+    putStrLn "Reading header of first file"
+    Right fileheader <- getHeader i
+    putStrLn "Attempt merge"
+    let ms = mergeFiles (readBlocksCheckHeader fileheader) (Boxed.fromList ins)
+    Stream.mapM_ (putStrLn . printEntityInfo) ms
+
+
+ where
+  getHeader f = do
+    stream <- streamOfFile f
+    second fst <$> getFile stream
+
+  -- TODO TODO TODO
+  readBlocksCheckHeader fileheader f = unsafePerformIO $ do
+    stream <- streamOfFile f
+    res <- getFile stream
+    case res of
+     -- TODO TODO TODO
+     Left err -> do
+      putStrLn ("Block error: reading file " <> show f <> " got error " <> show err)
+      exitFailure
+     Right (h,bs)
+      | h /= fileheader -> do
+        putStrLn ("Block error: reading file " <> show f <> " has different header")
+        exitFailure
+      | otherwise -> return $ Stream.mapMaybeM (return . maybeOfEither) bs
+
+  maybeOfEither (Left _) = Nothing
+  maybeOfEither (Right a) = Just a
+
+  printEntityInfo (Left err)  = "\n!!!Error: " <> show err <> "\n"
+  printEntityInfo (Right ent)
+   = "Entity: " <> show (emEntityHash ent) <> " : " <> show (emEntityId ent)
+   <> "\tValues: " <> show (Boxed.sum $ Boxed.map Unboxed.length $ emIndices ent)
+
 
 showBlocks :: Stream.Stream IO (Either DecodeError Block) -> IO ()
 showBlocks blocks = do
