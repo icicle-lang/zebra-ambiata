8f86d6cb1652f6aa776f650ee4b1344dd3e5995c
Amos Robinson <amos.robinson@gmail.com>
2016-11-24 17:22:47 +1100

    Foreign merge: better tests

--- test/Test/Zebra/Foreign/Merge.hs
+++ test/Test/Zebra/Foreign/Merge.hs
@@ -11,7 +11,7 @@ import           Control.Monad.Catch (bracket)
 import           Disorder.Core.IO (testIO)
 import           Disorder.Core.Run (disorderCheckEnvAll, ExpectedTestSpeed(..))
 import           Disorder.Jack (Property, counterexample)
-import           Disorder.Jack (gamble)
+import           Disorder.Jack (gamble, (===))
 import           Disorder.Jack (Jack, sized)
 import           Disorder.Jack (listOfN, maybeOf)
 
@@ -20,7 +20,7 @@ import           P
 import qualified Data.List as List
 import qualified Data.Vector as Boxed
 import           System.IO (IO)
-import           X.Control.Monad.Trans.Either (runEitherT)
+import           X.Control.Monad.Trans.Either (runEitherT, firstEitherT)
 import           Text.Show.Pretty (ppShow)
 
 import           Test.Zebra.Jack
@@ -31,11 +31,12 @@ import           Zebra.Foreign.Entity
 import           Zebra.Data.Block
 import           Zebra.Data.Core
 import           Zebra.Data.Encoding
+import           Zebra.Data.Entity
 import           Zebra.Data.Fact
 
 
 jEncodings :: Jack [Encoding]
-jEncodings = listOfN 2 2 jEncoding
+jEncodings = listOfN 0 5 jEncoding
 
 jFactForEntity :: (EntityHash, EntityId) -> Encoding -> AttributeId -> Jack Fact
 jFactForEntity eid encoding aid =
@@ -53,12 +54,18 @@ jFactsFor eid encs = sized $ \size -> do
   List.sort . List.concat <$> mapM (\(enc,aid) -> listOfN 0 maxFacts $ jFactForEntity eid enc aid) encs'
 
 
-foreignOfFacts :: Mempool.Mempool -> [Encoding] -> [Fact] -> IO (Block, Boxed.Vector CEntity)
-foreignOfFacts pool encs facts = do
+testForeignOfFacts :: Mempool.Mempool -> [Encoding] -> [Fact] -> IO (Block, Boxed.Vector CEntity)
+testForeignOfFacts pool encs facts = do
   let Right block    = blockOfFacts (Boxed.fromList encs) (Boxed.fromList facts)
   let Right entities = entitiesOfBlock block
   es' <- mapM (foreignOfEntity pool) entities
   return (block, es')
+
+testMergeFacts :: [Encoding] -> [Fact] -> Boxed.Vector Entity
+testMergeFacts encs facts =
+  let Right block    = blockOfFacts (Boxed.fromList encs) (Boxed.fromList $ List.sort facts)
+      Right entities = entitiesOfBlock block
+  in  entities
 
 
 prop_merge_1_entity_no_segfault :: Property
@@ -68,15 +75,40 @@ prop_merge_1_entity_no_segfault =
   gamble (jFactsFor eid encs) $ \facts1 ->
   gamble (jFactsFor eid encs) $ \facts2 ->
   testIO . bracket Mempool.create Mempool.free $ \pool -> withSegv (ppShow (eid, encs, facts1, facts2)) $ do
-    (b1,cs1) <- foreignOfFacts pool encs facts1
-    (b2,cs2) <- foreignOfFacts pool encs facts2
+    (b1,cs1) <- testForeignOfFacts pool encs facts1
+    (b2,cs2) <- testForeignOfFacts pool encs facts2
     let cs' = Boxed.zip cs1 cs2
     merged <- runEitherT $ mapM (uncurry $ mergeEntity pool) cs'
     -- Only checking segfault for now
     return $ counterexample (ppShow (b1,b2))
         $ case merged of
            Right _ -> True
-           Left _ -> True
+           Left _ -> False
+
+prop_merge_1_entity_check_result :: Property
+prop_merge_1_entity_check_result =
+  gamble jEntityHashId $ \eid ->
+  gamble jEncodings $ \encs ->
+  gamble (jFactsFor eid encs) $ \facts1 ->
+  gamble (jFactsFor eid encs) $ \facts2 ->
+  testIO . bracket Mempool.create Mempool.free $ \pool -> withSegv (ppShow (eid, encs, facts1, facts2)) $ do
+    let expect = testMergeFacts encs (facts1 <> facts2)
+    (b1,cs1) <- testForeignOfFacts pool encs facts1
+    (b2,cs2) <- testForeignOfFacts pool encs facts2
+    let cs' = Boxed.zip cs1 cs2
+    let err i = firstEitherT ppShow i
+    merged <- runEitherT $ do
+      ms <- err $ mapM (uncurry $ mergeEntity pool) cs'
+      err $ mapM entityOfForeign ms
+
+    return $ counterexample (ppShow (b1,b2))
+           $ case merged of
+              Right m'
+                | Boxed.length cs1 == 1 && Boxed.length cs2 == 1
+                -> expect === m'
+                | otherwise
+                -> Boxed.empty === m'
+              Left  e' -> counterexample e' False
 
 return []
 tests :: IO Bool
